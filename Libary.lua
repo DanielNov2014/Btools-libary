e63817de4716d2e967d54e2bdaee59aa9428c331
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-12-14 13:59:01 +0100

    Update Libary.lua

--- Libary.lua
+++ Libary.lua
@@ -1,9 +1,18 @@
--- inside MyLibrary.lua
-
+-- MyLibrary.lua
 local MyLibrary = {}
 local tool
 local remote
 
+-- Internal remote invoker
+local function _(args)
+    if remote then
+        remote:InvokeServer(unpack(args))
+    else
+        warn("Remote not initialized")
+    end
+end
+
+-- Setup: locate SyncAPI tool and remote
 function MyLibrary:Setup()
     local player = game.Players.LocalPlayer
 
@@ -19,31 +28,37 @@ function MyLibrary:Setup()
     end
 
     if tool then
-        remote = tool.SyncAPI.ServerEndpoint
+        remote = tool:FindFirstChild("SyncAPI") and tool.SyncAPI:FindFirstChild("ServerEndpoint")
+        if not remote then
+            warn("ServerEndpoint not found in SyncAPI")
+        end
     else
-        warn("No SyncAPI tool found")
+        warn("SyncAPI tool not found")
     end
 end
 
--- internal helper to invoke the remote
-local function _(args)
-    if remote then
-        remote:InvokeServer(unpack(args))
-    else
-        warn("Remote not set up")
-    end
-end
-
--- new SetName method
-function MyLibrary:SetName(part, stringg)
+-- SetName: rename a part via remote
+function MyLibrary:SetName(part, newName)
     local args = {
         [1] = "SetName",
         [2] = { [1] = part },
-        [3] = stringg
+        [3] = newName
     }
     _(args)
 end
 
+-- Internal CreatePart remote call
+local function CreatePart(cf, parent)
+    local args = {
+        [1] = "CreatePart",
+        [2] = "Normal",
+        [3] = cf,
+        [4] = parent
+    }
+    _(args)
+end
+
+-- CreatePart: spawn a server-side part
 function MyLibrary:CreatePart(pos, name)
     name = name or "Part"
 
@@ -62,13 +77,19 @@ function MyLibrary:CreatePart(pos, name)
 
     local createdpart = workspace.Terrain:FindFirstChild("Part")
     if createdpart then
-       self:SetName(createdpart, name)
+        self:SetName(createdpart, name)
     else
-        warn("No created part found under Terrain")
+        warn("Created part not found under Terrain")
     end
 
     part:Destroy()
     return createdpart
+end
+
+-- Optional: CreateWindow stub
+function MyLibrary:CreateWindow(options)
+    print("Creating window with title:", options.Title)
+    return {Title = options.Title}
 end
 
 return MyLibrary
